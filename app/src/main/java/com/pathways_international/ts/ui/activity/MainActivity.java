59b70aa1d101aac9a89d55be75f28ed85d368b10
Vikctar <vikc.steve@gmail.com>
2017-05-15 11:04:57 +0300

    Initial commit

---
+++ app/src/main/java/com/pathways_international/ts/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pathways_international.ts.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.pathways_international.ts.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
