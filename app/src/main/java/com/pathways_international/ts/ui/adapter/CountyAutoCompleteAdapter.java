2c99a50c2edf20f692ef6ba229c4db8737e2c89c
Vikctar <vikc.steve@gmail.com>
2017-05-15 18:07:00 +0300

    County adapter

---
+++ app/src/main/java/com/pathways_international/ts/ui/adapter/CountyAutoCompleteAdapter.java
@@ -0,0 +1,102 @@
+package com.pathways_international.ts.ui.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.TextView;
+
+import com.pathways_international.ts.R;
+import com.pathways_international.ts.ui.model.LocationModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by android-dev on 5/15/17.
+ */
+
+public class CountyAutoCompleteAdapter extends ArrayAdapter<LocationModel> {
+    String variant;
+    private List<LocationModel> mLocationModels;
+    private LayoutInflater layoutInflater;
+
+    private Filter mFilter = new Filter() {
+        @Override
+        public String convertResultToString(Object resultValue) {
+            return ((LocationModel) resultValue).getCounty_label();
+        }
+
+        @Override
+        protected Filter.FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+
+            if (constraint != null) {
+                ArrayList<LocationModel> suggestions = new ArrayList<LocationModel>();
+                for (LocationModel LocationModel : mLocationModels) {
+                    Log.d("filtering_", String.valueOf(constraint));
+                    // Note: change the "contains" to "startsWith" if you only want starting matches
+                    if (LocationModel.getCounty_label().startsWith(constraint.toString().toUpperCase())) {
+                        suggestions.add(LocationModel);
+                    }
+                }
+
+                results.values = suggestions;
+                results.count = suggestions.size();
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            clear();
+            if (results != null && results.count > 0) {
+                // we have filtered results
+                addAll((ArrayList<LocationModel>) results.values);
+            } else {
+                // no filter, add entire original list back in
+                addAll(mLocationModels);
+            }
+            notifyDataSetChanged();
+        }
+    };
+
+    public CountyAutoCompleteAdapter(Context context, int textViewResourceId, List<LocationModel> LocationModels, String variant) {
+        super(context, textViewResourceId, LocationModels);
+        // copy all the LocationModels into a master list
+        mLocationModels = new ArrayList<>(LocationModels.size());
+        mLocationModels.addAll(LocationModels);
+        layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.variant = variant;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+
+        if (view == null) {
+            view = layoutInflater.inflate(R.layout.autocomplete_row_item, null);
+        }
+
+        LocationModel LocationModel = getItem(position);
+
+        TextView name = (TextView) view.findViewById(R.id.textViewItem);
+        if (variant.equals("county")) {
+            name.setText(LocationModel.getCounty_label());
+        } else {
+            name.setText(LocationModel.getLocation_label());
+        }
+
+        return view;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return mFilter;
+    }
+
+}
